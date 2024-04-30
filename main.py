ba2860b94673a4cd07576579815c8c7bf34f7267
Vadim Shakirov <shakirov@ispras.ru>
2024-05-01 02:07:55 +0300

    add common functionality

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ class SteinerNode:
 		if not parent_node == None:
 			self.parent.childs.append(self)
 
+	def __str__(self):
+
+		return str(self.json)
+
 class SchemParams:
 
-	def __init__(self, tech_file, driver_node):
+	def __init__(self, tech_file):
 
 		json_data = None
 		with open(tech_file, "r") as file:
@@ -33,7 +37,7 @@ class SchemParams:
 		self.C_b = json_data["module"][0]["input"][0]["C"]
 		self.D_b = json_data["module"][0]["input"][0]["intrinsic_delay"]
 
-def fill_tree(parent, vertices, edges):
+def fill_tree(parent: SteinerNode, vertices, edges):
 
 	if parent.json["type"] == "t":
 		return
@@ -41,8 +45,8 @@ def fill_tree(parent, vertices, edges):
 	curr_id = parent.json["id"]
 	# find all childs
 	edges_to_childs = [edge for edge in edges if edge["vertices"][0] == curr_id]
-	print(edges_to_childs)
-	print("\n")
+#	print(edges_to_childs)
+#	print("\n")
 
 	for edge_to_child in edges_to_childs:
 
@@ -75,31 +79,44 @@ class ParamSolution:
 		self.Q = Q
 		self.curr_wirelen = previous_wire_len
 
-def get_wire_delay(params, wirelen, C_in):
+	def __str__(self):
+
+		return f"C: {self.C}, Q: {self.Q}, CW: {self.curr_wirelen}\n"
+
+def get_wire_delay(params: SchemParams, wirelen, C_in):
 
 	return (wirelen ** 2) * params.C_w * params.R_w / 2 + params.R_w * wirelen * C_in
 
-def get_buff_delay(params, C_in):
+def get_buff_delay(params: SchemParams, C_in):
 
 	return params.R_b * C_in + params.D_b
 
-def calc_branch(node, params, curr_solutions: [ParamSolution]): # Q - RAT
+def calc_branch(node: SteinerNode, params: SchemParams, curr_solutions: [ParamSolution]): # Q - RAT
 
 	wire_len = get_wire_len(node.path_to_parent)
 
-	init_size = len(curr_solutions)
-	edge_solutions = [] * init_size
+	print("wire_len:", wire_len)
+	print(*curr_solutions)
+	print(node)
 
+	init_size = len(curr_solutions)
+	edge_solutions = [[]] * init_size
+
+	print("init size: ", init_size)
+	print(edge_solutions)
+
+	# [start; finish); start from terminals
 	for i in range(wire_len): # or + 1 for s == t?
 
 		cs_best_to_buf = curr_solutions[0]
 		cs_index = 0
-		j = 0
-		for cs in curr_solutions:
 
+		for j in range(len(curr_solutions)):
+
+			cs = curr_solutions[j]
 			# set wire
 			cs.curr_wirelen += 1
-			cs.Q = cs.Q - get_wire_delay(params, wirelen, cs.C)
+			cs.Q = cs.Q - get_wire_delay(params, wire_len, cs.C)
 			cs.C = cs.C + params.C_w * cs.curr_wirelen # Is ok?
 
 			edge_solutions[j].append(0)
@@ -108,7 +125,6 @@ def calc_branch(node, params, curr_solutions: [ParamSolution]): # Q - RAT
 			if cs.Q - get_buff_delay(params, cs.C) <= cs_best_to_buf.Q - get_buff_delay(params, cs_best_to_buf.C):
 				cs_best_to_buf = cs
 				cs_index = j
-			j++
 
 		if cs_index < 0:
 			print("OH NO CS INDEX < 0!!!")
@@ -116,78 +132,63 @@ def calc_branch(node, params, curr_solutions: [ParamSolution]): # Q - RAT
 		edge_solutions.append(edge_solutions[cs_index])
 		edge_solutions[-1][-1] = 1 # Буффер есть
 
-		cs_buf = ParamSolution(params.C_b, cs_best_to_buf.Q - get_buff_delay(params, cs_best_to_buf.C, 0))
+		cs_buf = ParamSolution(params.C_b, cs_best_to_buf.Q - get_buff_delay(params, cs_best_to_buf.C), 0)
 		curr_solutions.append(cs_buf)
+
+	print("edge solutions:", len(edge_solutions))
+#	print("edge solutions:", edge_solutions)
 
 	node.options_to_parent = edge_solutions
 
 	return curr_solutions
 
-# Incorrect??
-def merge_two_childs(left_solutions, right_solutions, params):
+def merge_childs(childs_CQ_params_arr, params: SchemParams, num_of_childs):
 
 	solutions = []
 
-	i = 0
-	for curr_solution in [*left_solutions, *right_solutions]:
+	solution_index = 0
+	curr_target_child_index = 0
+	for child_CQ_params_arr in childs_CQ_params_arr:
 
-		curr_Q = curr_solution.Q
-		C_more_than_Q = [solution.C for solution in [*left_solutions, *right_solutions] if solution.Q >= curr_Q]
+		curr_CQ_param_index = 0
+		for child_CQ_param in child_CQ_params_arr:
 
-		if len(more_than_Q) == 0:
-			continue
+			curr_C = curr_solution.C
+			curr_Q = child_CQ_param.Q
 
-		min_index =  C_more_than_Q.index(min(C_more_than_Q))
-		curr_C = curr_solution.C + C_more_than_Q[min_index]
-		solutions.append(ParamSolution(curr_C, curr_Q, ??0))
-		i += 1
+			solution_ways = [None] * len(num_of_childs)
+			solution_ways[curr_target_child_index] = curr_CQ_param_index
+			
+			curr_other_child_index = 0
+			for other_CQ_params_arr in childs_CQ_params_arr:
+				# Very slow? need compare id's?
+				if other_CQ_params_arr == child_CQ_params_arr:
+					continue
 
+				C_more_than_Q = [CQ_param.C for CQ_param in other_CQ_params_arr if CQ_param.Q >= curr_Q]
+
+				if len(more_than_Q) == 0:
+					break # twice up - break
+
+				min_index = C_more_than_Q.index(min(C_more_than_Q))
+				curr_C += C_more_than_Q[min_index]
+			
+				solution_ways[curr_other_child_index] = min_index	
+				curr_other_child_index += 1
+			
+			solutions.append(ParamSolution(curr_C, curr_Q, 0)) # !!! может быть здесь верно выбирать больший или меньший
+			
+			node.options_from_merge.append(solution_ways) # must be solution_index
+
+			solution_index += 1
+			curr_CQ_param_index += 1
+		curr_target_child_index += 1
+
+	if len(solutions) != solution_index:
+		print("\nPANIC solution size don't equal solution index\n")
 	return solutions
 
-def merge_two_childs(left_solutions, right_solutions, params):
-
-	solutions = []
-
-	i = 0
-	for curr_solution in left_solutions:
-
-		curr_Q = curr_solution.Q
-		C_more_than_Q = [solution.C for solution in right_solutions if solution.Q >= curr_Q]
-
-		if len(more_than_Q) == 0:
-			continue
-
-		min_index =  C_more_than_Q.index(min(C_more_than_Q))
-		curr_C = curr_solution.C + C_more_than_Q[min_index]
-		solutions.append(ParamSolution(curr_C, curr_Q, 0)) # !!! может быть здесь верно выбирать больший или меньший
-		i += 1
-
-	i = 0
-	for curr_solution in right_solutions:
-
-		curr_Q = curr_solution.Q
-		C_more_than_Q = [solution.C for solution in left_solutions if solution.Q >= curr_Q]
-
-		if len(more_than_Q) == 0:
-			continue
-
-		min_index =  C_more_than_Q.index(min(C_more_than_Q))
-		curr_C = curr_solution.C + C_more_than_Q[min_index]
-		solutions.append(ParamSolution(curr_C, curr_Q, 0)) # !!! может быть здесь верно выбирать больший или меньший
-		i += 1
-
-	# ДОДЕЛАТЬ!!
-
-	return solutions
-
-def merge_childs(child_solutions):
-
-	for i in range(len(child_solutions) - 1):
-		child_solutions[0] = merge_two_childs(child_solutions[0], child_solutions[i])
-
-	return child_solutions[0]
-
-def bottom_up_order(node: SteinerNode):
+def bottom_up_order(node: SteinerNode, tech_params: SchemParams):
 
 	if node.json["type"] == "t":
 		return [ParamSolution(node.json["capacitance"], node.json["rat"], 0)]
@@ -195,13 +196,14 @@ def bottom_up_order(node: SteinerNode):
 	solutions = []
 
 	for child in node.childs:
-		solutions.append(calc_branch(bottom_up_order(child)))
+		solutions.append(calc_branch(child, tech_params, bottom_up_order(child, tech_params)))
 
-	solved = merge_solutions(solutions)
+	solved = merge_childs(solutions, tech_params, len(node.childs))
+	print(solved)
 
 	return solved
 
-def get_buffer_coords(node, index):
+def get_buffer_coords(node: SteinerNode, index):
 
 	edges = node.path_to_parent
 
@@ -230,7 +232,7 @@ def get_buffer_coords(node, index):
 			else:
 				return [edges[1].x + index, edges[1].y]
 
-def get_curr_edge_buffers(node, solution_id):
+def get_curr_edge_buffers(node: SteinerNode, solution_id):
 
 	buffers = []
 
@@ -243,7 +245,7 @@ def get_curr_edge_buffers(node, solution_id):
 
 	return buffers
 
-def get_curr_edge_split(node, buffers_coords):
+def get_curr_edge_split(node: SteinerNode, buffers_coords):
 
 	buffers_places = node.options_to_parent[solution_id]
 	new_vertices = [node.path_to_parent[0]]
@@ -256,7 +258,7 @@ def get_curr_edge_split(node, buffers_coords):
 	if num_segments == 2:
 		
 		curr_len = 0
-		if segments[0].x == segments[1].x
+		if segments[0].x == segments[1].x:
 			for i in range(wire_len):
 				# TODO ???
 				if buffers_places[i] == 1:
@@ -276,7 +278,7 @@ def get_curr_edge_split(node, buffers_coords):
 	else:
 		curr_len = 0
 		first_segment_len = abs(segments[0].x - segments[1].x + segments[0].y - segments[1].y)
-		if segments[0].x == segments[1].x
+		if segments[0].x == segments[1].x:
 			for i in range(first_segment_len):
 				# TODO ???
 				if buffers_places[i] == 1:
@@ -295,7 +297,7 @@ def get_curr_edge_split(node, buffers_coords):
 
 		curr_len = 0
 		second_segment_len = abs(segments[2].x - segments[1].x + segments[2].y - segments[1].y)
-		if segments[0].x == segments[1].x
+		if segments[0].x == segments[1].x:
 			for i in range(first_segment_len, second_segment_len):
 				# TODO ???
 				if buffers_places[i] == 1:
@@ -321,7 +323,7 @@ class EdgeBufferAndSplit:
 		self.buffers = []
 		self.vertices = []
 
-def get_solution_buffers(node, solution_id, buffers_and_edges):
+def get_solution_buffers(node: SteinerNode, solution_id, buffers_and_edges):
 
 	for i in range(len(node.childs)):
 		subtree_sol_id = node.options_from_merge[i]
@@ -332,16 +334,6 @@ def get_solution_buffers(node, solution_id, buffers_and_edges):
 	curr_buffers = get_curr_edge_buffers(node, solution_id)
 	buffers_and_edges.buffers.append(curr_buffers)
 	buffers_and_edges.vertices.append(get_curr_edge_split(node, curr_buffers))
-
-def print_buffers_from_solution(tree, solution_id):
-
-	root = tree.root
-
-	buffers_and_edges = EdgeBufferAndSplit()
-	# Root have only one child
-	get_solution_buffers(root.child[0], buffers_and_edges)
-
-	return buffers_and_edges.buffers, buffers_and_edges.vertices
 
 class SteinerTree:
 
@@ -364,7 +356,7 @@ class SteinerTree:
 
 		fill_tree(self.root, vertices, edges)
 
-		self.print()
+#		self.print()
 
 	def print(self):
 
@@ -381,7 +373,17 @@ class SteinerTree:
 
 			for child in node.childs:
 				q.append(child)
-		
+
+def print_buffers_from_solution(tree: SteinerTree, solution_id):
+
+	root = tree.root
+
+	buffers_and_edges = EdgeBufferAndSplit()
+	# Root have only one child
+	get_solution_buffers(root.child[0], buffers_and_edges)
+
+	return buffers_and_edges.buffers, buffers_and_edges.vertices
+
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description = "parse files")
@@ -393,7 +395,8 @@ if __name__ == "__main__":
 	tree = SteinerTree(args.test_filename)
 
 	# Get array of params
-	params = bottom_up_order(tree.root)
+	tech_params = SchemParams(args.technology_filename)
+	params = bottom_up_order(tree.root, tech_params)
 
 	# Get index of max RAT
 	max_index = 0
